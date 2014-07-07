c4484dc35967664c0b946ab866b03a83d6ebae6a
nhochberger <nico@irrwisch.net>
2014-07-07 22:00:54 +0200

    Prepared ImagejTargetFinder

---
+++ src/model/targetdetection/ImagejTargetFinder.java
@@ -0,0 +1,25 @@
+package model.targetdetection;
+
+import hochberger.utilities.application.session.BasicSession;
+import hochberger.utilities.application.session.SessionBasedObject;
+
+import java.util.List;
+
+public class ImagejTargetFinder extends SessionBasedObject implements
+		TargetFinder {
+
+	public ImagejTargetFinder(BasicSession session) {
+		super(session);
+	}
+
+	@Override
+	public void findTargets() {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public List<TargetPoint> getTargets() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
