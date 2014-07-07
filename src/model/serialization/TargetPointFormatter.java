9a14018d784a8bf424a9b133ea3c64cd412dde9d
nhochberger <nico@irrwisch.net>
2014-07-07 22:14:55 +0200

    changed TargetPointFormatter to comply with format given by customer

--- src/model/serialization/TargetPointFormatter.java
+++ src/model/serialization/TargetPointFormatter.java
@@ -7,15 +7,12 @@ public class TargetPointFormatter {
 	public TargetPointFormatter() {
 		super();
 	}
-	
+
 	public String format(int number, TargetPoint point) {
 		StringBuilder result = new StringBuilder();
-		result.append(String.valueOf(number))
-			.append(" [")
-			.append(String.valueOf(point.getxCoordinate()))
-			.append(":")
-			.append(String.valueOf(point.getyCoordinate()))
-			.append("]");
+		result.append(String.valueOf(number)).append(" : [")
+				.append(String.valueOf(point.getxCoordinate())).append(", ")
+				.append(String.valueOf(point.getyCoordinate())).append("]");
 		return result.toString();
 	}
 }
