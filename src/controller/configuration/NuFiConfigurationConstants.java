c50bd0c00e522fb5d8666709d3a852c4a08e3b62
nhochberger <nico@irrwisch.net>
2014-07-23 20:20:34 +0200

    added NuFiConfigurationConstants

---
+++ src/controller/configuration/NuFiConfigurationConstants.java
@@ -0,0 +1,12 @@
+package controller.configuration;
+
+import java.util.Arrays;
+import java.util.List;
+
+public interface NuFiConfigurationConstants {
+
+	public final static String SOURCE_FOLDER = "source.folder";
+	public final static String USED_CHANNELS = "used.channels";
+
+	public final static List<String> MANDATORY_ENTRIES = Arrays.asList(SOURCE_FOLDER, USED_CHANNELS);
+}
