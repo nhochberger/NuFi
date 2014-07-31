08174e9e84ff5e689ba5206597b25808e893cec3
nhochberger <nico@irrwisch.net>
2014-07-31 11:25:08 +0200

    workson #2
    slightly enhanced target detection

--- src/model/targetdetection/ImagejTargetFinder.java
+++ src/model/targetdetection/ImagejTargetFinder.java
@@ -4,6 +4,7 @@ import hochberger.utilities.application.session.BasicSession;
 import hochberger.utilities.application.session.SessionBasedObject;
 import ij.IJ;
 import ij.ImagePlus;
+import ij.plugin.ContrastEnhancer;
 import ij.plugin.filter.MaximumFinder;
 import ij.process.ImageProcessor;
 
@@ -11,6 +12,8 @@ import java.awt.Polygon;
 import java.io.File;
 import java.util.LinkedList;
 import java.util.List;
+
+import com.google.common.collect.Iterables;
 
 import controller.configuration.NuFiConfiguration;
 
@@ -27,16 +30,21 @@ public class ImagejTargetFinder extends SessionBasedObject implements TargetFind
 
 	@Override
 	public void findTargets() {
-		for (File image : this.configuration.getSourceFiles()) {
-			ImagePlus imagePlus = IJ.openImage(image.getAbsolutePath());
-			MaximumFinder maxFinder = new MaximumFinder();
-			ImageProcessor processor = imagePlus.getProcessor();
-			Polygon polygon = maxFinder.getMaxima(processor, 100, true);
-			for (int i = 0; i < polygon.xpoints.length; i++) {
-				this.targets.add(new TargetPoint(polygon.xpoints[i], polygon.ypoints[i]));
-			}
-			break;
+		Iterable<File> sourceFiles = this.configuration.getSourceFiles();
+		ImagePlus channel1 = IJ.openImage(Iterables.get(sourceFiles, 0).getAbsolutePath());
+		channel1.getProcessor().smooth();
+		channel1.getProcessor().smooth();
+		ContrastEnhancer enhancer = new ContrastEnhancer();
+		enhancer.setNormalize(true);
+		enhancer.stretchHistogram(channel1, 3);
+
+		MaximumFinder maxFinder = new MaximumFinder();
+		ImageProcessor processor = channel1.getProcessor();
+		Polygon polygon = maxFinder.getMaxima(processor, 20, true);
+		for (int i = 0; i < polygon.xpoints.length; i++) {
+			this.targets.add(new TargetPoint(polygon.xpoints[i], polygon.ypoints[i]));
 		}
+		channel1.show("channel1");
 	}
 
 	@Override
