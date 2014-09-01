cb2e855b7a834686c405909daa9fd8647b5bf005
nhochberger <nico@irrwisch.net>
2014-09-01 12:50:39 +0200

    workson #2 - working on in-depth analysis of ROIs - not yet working properly

--- src/model/targetdetection/ImagejTargetFinder.java
+++ src/model/targetdetection/ImagejTargetFinder.java
@@ -9,6 +9,7 @@ import ij.measure.ResultsTable;
 import ij.plugin.filter.ParticleAnalyzer;
 import ij.plugin.frame.RoiManager;
 import ij.process.AutoThresholder.Method;
+import ij.process.ImageProcessor;
 
 import java.awt.Rectangle;
 import java.util.LinkedList;
@@ -34,43 +35,80 @@ public class ImagejTargetFinder extends SessionBasedObject implements TargetFind
 	@Override
 	public void findTargets() {
 		NuFiImage nuFiImage = this.configuration.getNuFiImage();
-		ImagePlus channel1 = IJ.openImage(nuFiImage.getChannel1().getAbsolutePath());
-		ImagePlus channel3 = IJ.openImage(nuFiImage.getChannel3().getAbsolutePath());
+		RoiManager rois = findRoisIn(nuFiImage);
+		ImagePlus channel1 = findTargetsIn(nuFiImage, rois);
+		this.displayFactory.getResultDisplayer().displayResult(channel1, rois.getRoisAsArray(), this.targets);
+	}
+
+	private RoiManager findRoisIn(final NuFiImage image) {
+		ImagePlus channel3 = IJ.openImage(image.getChannel3().getAbsolutePath());
 		channel3.getProcessor().setAutoThreshold(Method.Default, true);
+		RoiManager manager = new RoiManager(true);
+		ResultsTable table = new ResultsTable();
 		int options = ParticleAnalyzer.ADD_TO_MANAGER | ParticleAnalyzer.IN_SITU_SHOW | ParticleAnalyzer.SHOW_OUTLINES | ParticleAnalyzer.EXCLUDE_EDGE_PARTICLES;
 		int measurements = 0;
-		ResultsTable table = new ResultsTable();
 		ParticleAnalyzer.setResultsTable(table);
-		RoiManager manager = new RoiManager(true);
 		ParticleAnalyzer.setRoiManager(manager);
 		ParticleAnalyzer analyzer = new ParticleAnalyzer(options, measurements, table, 5000d, 25000d);
 		boolean analysisResult = analyzer.analyze(channel3);
 		logger().info("Particle analysis result: " + analysisResult);
 		logger().info("Particle analysis found " + manager.getCount() + " ROIs.");
-		// until here: determining ROIs
+		return manager;
+	}
 
-		// now: find nucleoli
-		for (int i = 0; i < manager.getCount(); i++) {
-			manager.select(channel1, i);
-			Rectangle roiBounds = manager.getRoi(i).getBounds();
+	private ImagePlus findTargetsIn(final NuFiImage nuFiImage, final RoiManager rois) {
+		ImagePlus channel1 = IJ.openImage(nuFiImage.getChannel1().getAbsolutePath());
+		for (int i = 0; i < rois.getCount(); i++) {
+			logger().info("Beginning analysis of roi #" + i);
+			rois.select(channel1, i);
+			Rectangle roiBounds = rois.getRoi(i).getBounds();
 			int xOffset = (int) roiBounds.getX();
 			int yOffset = (int) roiBounds.getY();
 			ImagePlus workingImage = channel1.duplicate();
-			workingImage.updateAndDraw();
-			workingImage.getProcessor().setAutoThreshold(Method.MaxEntropy, true);
-			int roiOptions = ParticleAnalyzer.EXCLUDE_EDGE_PARTICLES;
-			int roiMeasurements = Measurements.CENTER_OF_MASS;
 			ResultsTable roiResults = new ResultsTable();
-			ParticleAnalyzer.setResultsTable(roiResults);
-			ParticleAnalyzer roiAnalyzer = new ParticleAnalyzer(roiOptions, roiMeasurements, roiResults, 50, 500);
-			System.err.println("analysis of roi #" + i + ": " + roiAnalyzer.analyze(workingImage));
+			analyzeSingleRoi(workingImage, roiResults);
+			int roiParticleCounter = roiResults.getCounter();
+			logger().info("Found " + roiParticleCounter + " targets.");
+			if (2 > roiParticleCounter) {
+				performInDepthAnalysisOf(workingImage, roiResults);
+			}
 			for (int j = 0; j < roiResults.getCounter(); j++) {
 				int x = (int) (roiResults.getValue("XM", j) + xOffset);
 				int y = (int) (roiResults.getValue("YM", j) + yOffset);
 				this.targets.add(new TargetPoint(x, y));
 			}
 		}
-		this.displayFactory.getResultDisplayer().displayResult(channel1, manager.getRoisAsArray(), this.targets);
+		return channel1;
+	}
+
+	private void performInDepthAnalysisOf(final ImagePlus workingImage, final ResultsTable roiResults) {
+		int autoThreshold = workingImage.getProcessor().getAutoThreshold();
+		logger().info("Found no targets with automatic approach (auto threshold: " + autoThreshold + "). Performing indepth analysis.");
+		int highestThreshold = (int) (autoThreshold * 1.3);
+		highestThreshold = highestThreshold <= 255 ? highestThreshold : 255;
+		int lowestThreshold = (int) (autoThreshold * 0.7);
+		logger().info("Beginning indepth analysis with threshold between " + lowestThreshold + " and " + highestThreshold);
+		for (int threshold = highestThreshold; lowestThreshold < threshold; threshold -= 10) {
+			ImagePlus duplicate = workingImage.duplicate();
+			duplicate.getProcessor().setThreshold(threshold, 255f, ImageProcessor.RED_LUT);
+			int roiOptions = ParticleAnalyzer.EXCLUDE_EDGE_PARTICLES;
+			int roiMeasurements = Measurements.CENTER_OF_MASS;
+			ParticleAnalyzer.setResultsTable(roiResults);
+			ParticleAnalyzer roiAnalyzer = new ParticleAnalyzer(roiOptions, roiMeasurements, roiResults, 25, 500);
+			boolean roiAnalysisResult = roiAnalyzer.analyze(duplicate);
+			logger().info("Result of analysis: " + roiAnalysisResult);
+			System.err.println("count: " + roiResults.getCounter());
+		}
+	}
+
+	private void analyzeSingleRoi(final ImagePlus workingImage, final ResultsTable resultTable) {
+		workingImage.getProcessor().setAutoThreshold(Method.MaxEntropy, true);
+		int roiOptions = ParticleAnalyzer.EXCLUDE_EDGE_PARTICLES;
+		int roiMeasurements = Measurements.CENTER_OF_MASS;
+		ParticleAnalyzer.setResultsTable(resultTable);
+		ParticleAnalyzer roiAnalyzer = new ParticleAnalyzer(roiOptions, roiMeasurements, resultTable, 50, 500);
+		boolean roiAnalysisResult = roiAnalyzer.analyze(workingImage);
+		logger().info("Result of analysis: " + roiAnalysisResult);
 	}
 
 	@Override
--- src/model/targetdetection/TargetPoint.java
+++ src/model/targetdetection/TargetPoint.java
@@ -5,22 +5,34 @@ public class TargetPoint {
 	private final int xCoordinate;
 	private final int yCoordinate;
 
-	public TargetPoint(int xCoordinate, int yCoordinate) {
+	public TargetPoint(final int xCoordinate, final int yCoordinate) {
 		super();
 		this.xCoordinate = xCoordinate;
 		this.yCoordinate = yCoordinate;
 	}
 
 	public int getxCoordinate() {
-		return xCoordinate;
+		return this.xCoordinate;
 	}
 
 	public int getyCoordinate() {
-		return yCoordinate;
+		return this.yCoordinate;
 	}
 
 	@Override
 	public String toString() {
-		return "(" + xCoordinate + ":" + yCoordinate + ")";
+		return "(" + this.xCoordinate + ":" + this.yCoordinate + ")";
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		if (null == obj) {
+			return false;
+		}
+		if (!(obj instanceof TargetPoint)) {
+			return false;
+		}
+		TargetPoint other = (TargetPoint) obj;
+		return other.getxCoordinate() == getxCoordinate() && other.getyCoordinate() == getyCoordinate();
 	}
 }
